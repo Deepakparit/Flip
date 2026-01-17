112176d8bcae70b687cf89018292cc33c89f1c50
Deepak <deepakdparit4449@gmail.com>
2026-01-17 18:25:30 +0530

    Update App.java

--- src/main/java/flip/App.java
+++ src/main/java/flip/App.java
@@ -5,7 +5,7 @@ package flip;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!")
+        System.out.println("Hello World!");
         
     }
 }
